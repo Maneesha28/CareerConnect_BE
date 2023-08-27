9238768e5d87365b08342d755e0c96dd13de9fdf
SumaiyaAny <sumaiyaany863@gmail.com>
2023-08-27 14:00:10 +0600

    about, linkedin_link added

--- backend/routes/users/jobseeker.js
+++ backend/routes/users/jobseeker.js
@@ -6,7 +6,8 @@ const { verify } = require('../../middlewares/user-verification');
 
 router.put('', verifyJobseeker, async (req, res) => {
     await DB_jobseeker.editJobseeker(req.body.name, req.body.gender, req.body.profile_pic, req.body.date_of_birth,
-            req.body.nationality, req.body.nid, req.body.address, req.body.phone_no, req.user.jobseeker_id);   
+            req.body.nationality, req.body.nid, req.body.address, req.body.phone_no,req.body.github_link,
+            req.body.about,req.body.linkedin_link, req.user.jobseeker_id);   
     res.send({"status" : "Jobseeker edited"});
 });
 
